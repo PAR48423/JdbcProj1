01ffe1d62be69619ce3caf834cfc83348add62d0
dev1 <dev1@gmail.com>
2021-08-19 16:26:34 +0530

    Test TEAM Leader

---
+++ JdbcProj1/src/co/pa/test/UtilityTest.java
@@ -0,0 +1,10 @@
+package co.pa.test;
+
+public class UtilityTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            System.out.print("Helllo test");
+	}
+
+}
