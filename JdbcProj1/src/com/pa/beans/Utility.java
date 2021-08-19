1848aa6e35cedcb45418e45586768c8add719341
dev1 <dev1@gmail.com>
2021-08-19 16:22:27 +0530

    dev1 initial

--- JdbcProj1/src/com/pa/beans/Utility.java
+++ JdbcProj1/src/com/pa/beans/Utility.java
@@ -1,6 +1,11 @@
 package com.pa.beans;
 
+import java.util.Date;
+
 public class Utility {
+	public Date getdate() {
+		return new Date();
+	}
 
 	public static void main(String[] args) {
           String s1="HIi how are You";
